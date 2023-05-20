9c6c8ce769e90df357fc01543319deaa21241306
Shibankur <shibankur.das01@gmail.com>
2023-05-20 22:18:08 +0530

    Updated with Protection on 20-05-2023
    Updated with Protection on 20-05-2023.

---
+++ PDFGenerator/src/com/generate/pdf/util/ApplicationConstant.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.generate.pdf.util;
+
+/**
+ * @author ShibankurD
+ *
+ */
+public interface ApplicationConstant {
+	
+	public static String USER_PASSWORD = "1234567";
+	public static String OWNER_PASSWORD = "shibankur";
+}
