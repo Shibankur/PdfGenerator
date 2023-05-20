b169ac8669548ba06d806379ec41742e74c0557e
Shibankur <shibankur.das01@gmail.com>
2023-05-20 16:21:58 +0530

    Jar added on 20-05-2023
    Jar added on 20-05-2023

--- PDFGenerator/src/com/generate/pdf/GeneratePDFAppMain.java
+++ PDFGenerator/src/com/generate/pdf/GeneratePDFAppMain.java
@@ -3,12 +3,31 @@
  */
 package com.generate.pdf;
 
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;  
+import org.apache.pdfbox.pdmodel.PDPage;  
+
 /**
  * @author ShibankurD
  *
  */
 public class GeneratePDFAppMain {
-	public static void main(String []args) {
+	public static void main(String []args) throws IOException {
 		System.out.println("Inside main class.");
-	}
+		PDDocument pdfdoc= new PDDocument();  
+		pdfdoc.addPage(new PDPage());  
+		//path where the PDF file will be store  
+		try {
+			pdfdoc.save("Sample.pdf");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		//prints the message if the PDF is created successfully   
+		System.out.println("PDF created");  
+		//closes the document  
+		pdfdoc.close();  
+	}  
 }
+
